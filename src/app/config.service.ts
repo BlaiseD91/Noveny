dbe4e3f20d2d72f8422da5fb5af70277887cc6fc
Székely Balázs <szekelyb91@gmail.com>
2023-11-30 19:23:24 +0100

    start: 2 services, 3 components

---
+++ src/app/config.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigService {
+
+  config:any;
+  private configSub = new BehaviorSubject({});
+
+
+  constructor(private http:HttpClient) {
+    this.setLanguage('hu');
+  }
+
+  getConfig(){
+    return this.configSub;
+  }
+
+  setLanguage(lang:string){
+    this.http.get('../assets/lang_' + lang + '.json').subscribe(
+      (response) => {
+        this.config=response;
+        this.configSub.next(response);
+        console.log(this.config);
+      }
+    );
+  }
+}
---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ConfigService } from '../config.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+
+  messages:any;
+
+  constructor(private config:ConfigService) {
+    config.getConfig().subscribe(
+      (res:any) => this.messages = res.nav
+    )
+  }
+}
